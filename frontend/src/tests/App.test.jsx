cd9d956f0d96dcaff0bcf78ef46f78f4fbb108d4
Jon Kerry <jonathan.kerry@congenica.com>
2019-08-26 19:49:08 +0100

    Add a test for a simple component; install prop-types

---
+++ frontend/src/tests/App.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+
+import App from '../App';
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('Renders text as expected', () => {
+  act(() => {
+    render(<App name="Bob" />, container);
+  });
+  const spanElement = container.getElementsByTagName('span')[0];
+  expect(spanElement.innerHTML).toEqual('Hello Bob');
+});
