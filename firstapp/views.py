af774f179ab6e7d3d6ecaef809fee08fa7c4fde4
Jon Kerry <jonathan.kerry@congenica.com>
2019-08-28 21:58:39 +0100

    Add a dummy test for Django

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home_page(request):  # pylint: disable=unused-argument
+    return HttpResponse("<html><title>To-Do lists</title></html>")
--- fs_project/urls.py
+++ fs_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from firstapp import views
+
 urlpatterns = [  # pylint: disable=invalid-name
     path('admin/', admin.site.urls),
+    path('', views.home_page, name="home")
 ]
